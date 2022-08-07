41b29e3114973e141aaccfbe41836eee3c8a0db7
TI-AviBerko <avi@ti.com>
2022-08-07 23:04:45 +0300

    Update sync_from_tflite_micro.sh

--- scripts/sync_from_tflite_micro.sh
+++ scripts/sync_from_tflite_micro.sh
@@ -49,7 +49,7 @@ mv "${TEMP_DIR}/tflm-out/tensorflow" tensorflow
 # For this repo we are forking both the models and the examples.
 rm -rf tensorflow/lite/micro/models
 
-/bin/cp -r "${TEMP_DIR}"/tflm-out/third_party/*ls . third_party/
+/bin/cp -r "${TEMP_DIR}"/tflm-out/third_party/* third_party/
 
 # Copy the CMSIS Device include directory, for some reason this is not the create_tflm_tree.py ...
 mkdir -p third_party/cmsis/Device/ARM/ARMCM${ARM_CPU}/Include
